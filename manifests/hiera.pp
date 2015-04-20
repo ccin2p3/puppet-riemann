a37c81022b87a050ce6b93a713de389c6d924591
Fabien Wernli <wernli@in2p3.fr>
2015-04-20 14:12:01 +0200

    implement variable prefix to hiera

--- manifests/hiera.pp
+++ manifests/hiera.pp
@@ -1,6 +1,9 @@
 #
 
-class riemann::hiera {
+class riemann::hiera (
+  $hiera_prefix = ''
+)
+  {
   riemann::hiera::create_resources {
     ['config::fragment','listen','let','streams','stream','stream::publish','subscribe']:
   }
@@ -8,10 +11,11 @@ class riemann::hiera {
   
 define riemann::hiera::create_resources {
   $rsname = "riemann::${title}"
+  $varname = "${hiera_prefix}${rsname}"
   create_resources (
     $rsname,
     hiera_hash(
-      $rsname,
+      $varname,
       {}
     )
   )
