ac9e745b603a0756d8a9cc7b9908f7db5f5f54e5
Fabien Wernli <wernli@in2p3.fr>
2015-04-21 18:18:45 +0200

    fix bug where the literal "false" was used as options

--- manifests/listen.pp
+++ manifests/listen.pp
@@ -1,17 +1,13 @@
 #
 define riemann::listen (
   $type = $title,
-  $options = false
+  $options = ''
 ) {
   if $riemann::debug {
     $debug_header = ";begin riemann::listen `${title}`\n"
     $debug_footer = "\n;end riemann::listen `${title}`"
   }
-  if $options {
-    #$options_str = [' :',join(join_keys_to_values($options,' '),' :')]
-  }
   riemann::config::fragment { "listen_${title}":
-    #content => "${debug_header}(${type}-server${options_str})${debug_footer}",
     content => [ "${type}-server", $options ],
     order   => '15'
   }
