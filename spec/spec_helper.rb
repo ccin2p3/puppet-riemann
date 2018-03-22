fa7dd664886b734078539a294d22b7efac11f076
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <wernli@in2p3.fr>
2018-03-22 15:29:21 +0100

    refresh spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,34 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
 
-# common facts to all supported OS
-# e.g. mocking concat_basedir can be handy
+
+require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
+
+begin
+  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
+rescue LoadError => loaderror
+  warn "Could not require spec_helper_local: #{loaderror.message}"
+end
+
+include RspecPuppetFacts
+
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
+}
+
+default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
+default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+
+if File.exist?(default_facts_path) && File.readable?(default_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
+end
+
+if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+end
 
 RSpec.configure do |c|
-  c.default_facts = {
-    concat_basedir: '/DIR'
-  }
+  c.default_facts = default_facts
 end
 
 # specific facts per supported OS
