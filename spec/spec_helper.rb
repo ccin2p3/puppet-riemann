200b3e2ba76c7f6e601b25a04bc4ab9612d55d71
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <wernli@in2p3.fr>
2019-08-07 14:17:30 +0200

    WIP: unknow shit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,7 @@
-
-
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 
-begin
-  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
-rescue LoadError => loaderror
-  warn "Could not require spec_helper_local: #{loaderror.message}"
-end
+require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
 
 include RspecPuppetFacts
 
@@ -16,87 +10,38 @@ default_facts = {
   facterversion: Facter.version,
 }
 
-default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
-default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+default_fact_files = [
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml')),
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml')),
+]
 
-if File.exist?(default_facts_path) && File.readable?(default_facts_path)
-  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
-end
+default_fact_files.each do |f|
+  next unless File.exist?(f) && File.readable?(f) && File.size?(f)
 
-if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
-  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+  begin
+    default_facts.merge!(YAML.safe_load(File.read(f), [], [], true))
+  rescue => e
+    RSpec.configuration.reporter.message "WARNING: Unable to load #{f}: #{e}"
+  end
 end
 
 RSpec.configure do |c|
-  c.mock_with :rspec
   c.default_facts = default_facts
+  c.before :each do
+    # set to strictest setting for testing
+    # by default Puppet runs at warning level
+    Puppet.settings[:strict] = :warning
+  end
+  c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
+  c.after(:suite) do
+  end
 end
 
-# specific facts per supported OS
-#
+def ensure_module_defined(module_name)
+  module_name.split('::').reduce(Object) do |last_module, next_module|
+    last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module, false)
+    last_module.const_get(next_module, false)
+  end
+end
 
-@os_fixtures = {
-  'EL6' => {
-    facts: {
-      operatingsystem: 'Scientific',
-      osfamily: 'RedHat',
-      operatingsystemmajrelease: 6,
-      os: { family: 'RedHat', release: { major: 6 } },
-    },
-    params: {
-      package_name: 'riemann',
-      service_name: 'riemann',
-      config_dir: '/etc/riemann',
-      init_config_file: '/etc/sysconfig/riemann',
-      reload_command: '/sbin/service riemann reload',
-      log_file: '/var/log/riemann/riemann.log',
-    },
-  },
-  'EL7' => {
-    facts: {
-      operatingsystem: 'CentOS',
-      osfamily: 'RedHat',
-      operatingsystemmajrelease: 7,
-      os: { family: 'RedHat', release: { major: 7 } },
-    },
-    params: {
-      package_name: 'riemann',
-      service_name: 'riemann',
-      config_dir: '/etc/riemann',
-      init_config_file: '/etc/sysconfig/riemann',
-      reload_command: 'kill -HUP $(</var/run/riemann.pid)',
-      log_file: '/var/log/riemann/riemann.log',
-    },
-  },
-  'Debian8' => {
-    facts: {
-      operatingsystem: 'Debian',
-      osfamily: 'Debian',
-      operatingsystemmajrelease: 8,
-      os: { family: 'Debian', release: { major: 8 } },
-    },
-    params: {
-      package_name: 'riemann',
-      service_name: 'riemann',
-      config_dir: '/etc/riemann',
-      init_config_file: '/etc/default/riemann',
-      reload_command: '/usr/sbin/service riemann reload',
-      log_file: '/var/log/riemann/riemann.log',
-    },
-  },
-}
-
-# add other combinations if you want to support a new OS e.g.
-#
-# 'MyOS-42' => @common_facts.merge({
-#    :operatingsystem => 'OpenSolaris',
-#    :osfamily => 'Solaris',
-#    :operatingsystemmajrelease => 12
-#  })
-
-# If you want to fetch all values from hiera
-#  e.g. because you're testing code that uses explicit hiera lookups
-#
-# RSpec.configure do |c|
-#  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
-# end
+# 'spec_overrides' from sync.yml will appear below this line
