71d9f9b22264c37e334026cc76e3f64c032f1fe7
Romain Tartière <romain@blogreen.org>
2022-08-12 14:44:10 -1000

    Update Unit tests to cope with new setup

--- spec/classes/logging_spec.rb
+++ spec/classes/logging_spec.rb
@@ -3,19 +3,15 @@ require 'spec_helper'
 os_fixtures = @os_fixtures
 
 describe 'riemann::logging' do
-  context 'supported operating systems' do
-    os_fixtures.each do |osname, osfixtures|
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
       describe 'without any parameters' do
         let(:params) { {} }
 
-        describe "on #{osname}" do
-          let(:facts) do
-            osfixtures[:facts]
-          end
-
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to contain_riemann__config__fragment('riemann::logging').with_content(%r{logging\/init}) }
-        end
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_riemann__config__fragment('riemann::logging').with_content(%r{logging\/init}) }
       end
     end
   end
