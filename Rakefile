f878869ddc8276d85b86d31c4e78ad1a10206ce9
Romain Tartière <romain@blogreen.org>
2022-08-17 07:40:11 -1000

    Update using ModuleSync

--- Rakefile
+++ Rakefile
@@ -1,20 +1,72 @@
-require 'rubygems'
-require 'puppetlabs_spec_helper/rake_tasks'
-require 'puppet-lint/tasks/puppet-lint'   
+# Managed by modulesync - DO NOT EDIT
+# https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
 
-PuppetLint.configuration.fail_on_warnings
-PuppetLint.configuration.send('disable_80chars')
-PuppetLint.configuration.send('disable_single_quote_string_with_variables')
-PuppetLint.configuration.relative = true
-PuppetLint.configuration.send('disable_140chars')
-
-ignore_paths = ['vendor/**/*', 'spec/**/*', 'smoke/**/*', 'pkg/**/*']
-
-PuppetSyntax.exclude_paths = ignore_paths
-
-#PuppetLint.configuration.ignore_paths = ignore_paths
-PuppetLint::RakeTask.new :lint do |config|
-  config.ignore_paths = ignore_paths
+# Attempt to load voxpupuli-test (which pulls in puppetlabs_spec_helper),
+# otherwise attempt to load it directly.
+begin
+  require 'voxpupuli/test/rake'
+rescue LoadError
+  begin
+    require 'puppetlabs_spec_helper/rake_tasks'
+  rescue LoadError
+  end
 end
 
-task :default => [:spec, :lint]
+# load optional tasks for acceptance
+# only available if gem group releases is installed
+begin
+  require 'voxpupuli/acceptance/rake'
+rescue LoadError
+end
+
+# load optional tasks for releases
+# only available if gem group releases is installed
+begin
+  require 'voxpupuli/release/rake_tasks'
+rescue LoadError
+end
+
+desc "Run main 'test' task and report merged results to coveralls"
+task test_with_coveralls: [:test] do
+  if Dir.exist?(File.expand_path('../lib', __FILE__))
+    require 'coveralls/rake/task'
+    Coveralls::RakeTask.new
+    Rake::Task['coveralls:push'].invoke
+  else
+    puts 'Skipping reporting to coveralls.  Module has no lib dir'
+  end
+end
+
+desc 'Generate REFERENCE.md'
+task :reference, [:debug, :backtrace] do |t, args|
+  patterns = ''
+  Rake::Task['strings:generate:reference'].invoke(patterns, args[:debug], args[:backtrace])
+end
+
+begin
+  require 'github_changelog_generator/task'
+  require 'puppet_blacksmith'
+  GitHubChangelogGenerator::RakeTask.new :changelog do |config|
+    metadata = Blacksmith::Modulefile.new
+    config.future_release = "v#{metadata.version}" if metadata.version =~ /^\d+\.\d+.\d+$/
+    config.header = "# Changelog\n\nAll notable changes to this project will be documented in this file.\nEach new release typically also includes the latest modulesync defaults.\nThese should not affect the functionality of the module."
+    config.exclude_labels = %w{duplicate question invalid wontfix wont-fix modulesync skip-changelog}
+    config.user = 'ccin2p3'
+    config.project = "puppet-#{metadata.metadata['name'].split('-').last}"
+  end
+
+  # Workaround for https://github.com/github-changelog-generator/github-changelog-generator/issues/715
+  require 'rbconfig'
+  if RbConfig::CONFIG['host_os'] =~ /linux/
+    task :changelog do
+      puts 'Fixing line endings...'
+      changelog_file = File.join(__dir__, 'CHANGELOG.md')
+      changelog_txt = File.read(changelog_file)
+      new_contents = changelog_txt.gsub(%r{\r\n}, "\n")
+      File.open(changelog_file, "w") {|file| file.puts new_contents }
+    end
+  end
+
+rescue LoadError
+end
+# vim: syntax=ruby
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,102 +1,17 @@
+# frozen_string_literal: true
 
+# Managed by modulesync - DO NOT EDIT
+# https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
 
-require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet-facts'
+# puppetlabs_spec_helper will set up coverage if the env variable is set.
+# We want to do this if lib exists and it hasn't been explicitly set.
+ENV['COVERAGE'] ||= 'yes' if Dir.exist?(File.expand_path('../lib', __dir__))
 
-begin
-  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
-rescue LoadError => loaderror
-  warn "Could not require spec_helper_local: #{loaderror.message}"
+require 'voxpupuli/test/spec_helper'
+
+if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
+  facts = YAML.safe_load(File.read(File.join(__dir__, 'default_module_facts.yml')))
+  facts&.each do |name, value|
+    add_custom_fact name.to_sym, value
+  end
 end
-
-include RspecPuppetFacts
-
-default_facts = {
-  puppetversion: Puppet.version,
-  facterversion: Facter.version,
-}
-
-default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
-default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
-
-if File.exist?(default_facts_path) && File.readable?(default_facts_path)
-  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
-end
-
-if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
-  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
-end
-
-RSpec.configure do |c|
-  c.mock_with :rspec
-  c.default_facts = default_facts
-end
-
-# specific facts per supported OS
-#
-
-@os_fixtures = {
-  'EL6' => {
-    facts: {
-      operatingsystem: 'Scientific',
-      osfamily: 'RedHat',
-      operatingsystemmajrelease: 6,
-      os: { family: 'RedHat', release: { major: 6 } },
-    },
-    params: {
-      package_name: 'riemann',
-      service_name: 'riemann',
-      config_dir: '/etc/riemann',
-      init_config_file: '/etc/sysconfig/riemann',
-      reload_command: '/sbin/service riemann reload',
-      log_file: '/var/log/riemann/riemann.log',
-    },
-  },
-  'EL7' => {
-    facts: {
-      operatingsystem: 'CentOS',
-      osfamily: 'RedHat',
-      operatingsystemmajrelease: 7,
-      os: { family: 'RedHat', release: { major: 7 } },
-    },
-    params: {
-      package_name: 'riemann',
-      service_name: 'riemann',
-      config_dir: '/etc/riemann',
-      init_config_file: '/etc/sysconfig/riemann',
-      reload_command: 'kill -HUP $(</var/run/riemann.pid)',
-      log_file: '/var/log/riemann/riemann.log',
-    },
-  },
-  'Debian8' => {
-    facts: {
-      operatingsystem: 'Debian',
-      osfamily: 'Debian',
-      operatingsystemmajrelease: 8,
-      os: { family: 'Debian', release: { major: 8 } },
-    },
-    params: {
-      package_name: 'riemann',
-      service_name: 'riemann',
-      config_dir: '/etc/riemann',
-      init_config_file: '/etc/default/riemann',
-      reload_command: '/usr/sbin/service riemann reload',
-      log_file: '/var/log/riemann/riemann.log',
-    },
-  },
-}
-
-# add other combinations if you want to support a new OS e.g.
-#
-# 'MyOS-42' => @common_facts.merge({
-#    :operatingsystem => 'OpenSolaris',
-#    :osfamily => 'Solaris',
-#    :operatingsystemmajrelease => 12
-#  })
-
-# If you want to fetch all values from hiera
-#  e.g. because you're testing code that uses explicit hiera lookups
-#
-# RSpec.configure do |c|
-#  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
-# end
